2859a98083fdf0929f3bfcb59b6a758c39584efe
nvaccina <niccolovac@gmail.com>
2023-04-18 12:12:09 +0200

    Function printClock

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -8,38 +8,49 @@ createApp({
     return{
       contacts,
       counter: 0,
-      clock:null,
+      clock:'',
       contact:'',
       newMessageString:'',
     }
   },
   methods:{
+    //Funzione nuovo messaggio inviato
     addMessage(){
+      this.printClock()
 
       const newMessage = {
-        date: '18/04/2023',
+        date: this.clock,
         message: this.newMessageString,
         status: 'sent',
       }
+      console.log(newMessage);
 
       this.contacts[this.counter].messages.push(newMessage);
       this.newMessageString = '';
+
       setTimeout(() => {
         this.addAnswerMessage()
       },1000)
     },
 
+    //Funzione messaggio automatico ricevuto after 1s
     addAnswerMessage(){
+      this.printClock()
+
       const newAnswerMessage = {
-        date: '18/04/2023',
+        date: this.clock,
         message: 'Si, hai proprio ragione',
         status: 'received',
       }
       this.contacts[this.counter].messages.push(newAnswerMessage);
-
     },
 
-    
-  }
+    //Funzione orario attuale al momento dell'invio messaggio
+    printClock(){
+      this.clock = dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
+    },
+
+
+  },
 
 }).mount('#app')
