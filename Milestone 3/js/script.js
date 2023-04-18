b02f0d07d0b6715448ad9d5456c31fe13d0d54ea
nvaccina <niccolovac@gmail.com>
2023-04-18 11:50:13 +0200

    Funzione risposta automatica dopo 1s

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -1,11 +1,14 @@
 const {createApp} = Vue;
 import contacts from './db.js'
 
+const dt = luxon.DateTime
+
 createApp({
   data(){
     return{
       contacts,
       counter: 0,
+      clock:null,
       contact:'',
       newMessageString:'',
     }
@@ -18,12 +21,25 @@ createApp({
         message: this.newMessageString,
         status: 'sent',
       }
-      console.log(newMessage);
 
       this.contacts[this.counter].messages.push(newMessage);
       this.newMessageString = '';
-      console.log(contacts);
-    }
+      setTimeout(() => {
+        this.addAnswerMessage()
+      },1000)
+    },
+
+    addAnswerMessage(){
+      const newAnswerMessage = {
+        date: '18/04/2023',
+        message: 'Si, hai proprio ragione',
+        status: 'received',
+      }
+      this.contacts[this.counter].messages.push(newAnswerMessage);
+
+    },
+
+    
   }
 
 }).mount('#app')
